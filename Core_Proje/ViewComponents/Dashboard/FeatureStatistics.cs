8bd3f9617f50f9261ef96789247409aaba585a9e
Ozgun-Sahin <ozgun_sahin@outlook.com>
2022-11-12 13:40:37 +0300

    Thema Changed

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}

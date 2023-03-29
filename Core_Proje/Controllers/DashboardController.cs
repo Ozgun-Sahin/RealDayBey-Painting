a20258565e8819a7678e12521eb7c0a199d9add2
Ozgun-Sahin <ozgun_sahin@outlook.com>
2023-03-29 23:14:44 +0300

    Pie Chart added to Admin Dashboard

--- Core_Proje/Controllers/DashboardController.cs
+++ Core_Proje/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
@@ -6,6 +8,23 @@ namespace Core_Proje.Controllers
     {
         public IActionResult DashboardIndex()
         {
+            ProjectManager projectManager = new ProjectManager(new EFProjectDal());
+
+            var thisMounthsProjects = projectManager.GetListProjectsByCreationDate();
+
+            int totalIncome = 0;
+            int totalOutcome = 0;
+
+            foreach (var item in thisMounthsProjects)
+            {
+                totalIncome = (int)(totalIncome + item.Price);
+                totalOutcome = (int)(totalOutcome + item.Expence);
+            }
+
+            ViewBag.totalIncome = totalIncome;
+            ViewBag.totalOutcome = totalOutcome;
+            ViewBag.net = totalIncome - totalOutcome;
+
             return View();
         }
     }

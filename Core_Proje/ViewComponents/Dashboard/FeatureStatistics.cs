dd7608b58e204fe38928897e842abf7cef434780
Ozgun-Sahin <ozgun_sahin@outlook.com>
2023-04-10 09:57:19 +0300

    Message Class removed

--- Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -9,8 +9,8 @@ namespace Core_Proje.ViewComponents.Dashboard
         public IViewComponentResult Invoke()
         {
             ViewBag.v1 = c.Skills.Count();
-            ViewBag.v2 = c.Messages.Where(x=>x.Status == false).Count();
-            ViewBag.v3 = c.Messages.Where(x=>x.Status == true).Count();
+            //ViewBag.v2 = c.Messages.Where(x=>x.Status == false).Count();
+            //ViewBag.v3 = c.Messages.Where(x=>x.Status == true).Count();
             ViewBag.v4 = c.Experiences.Count();
             return View();
         }
